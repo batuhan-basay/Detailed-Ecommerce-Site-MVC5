80626266a40d6c2e119298ed07b6036c80297575
BatuhanBaşay <batuhan.basay@hotmail.com>
2022-04-06 03:20:01 +0300

    start

---
+++ MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/KategoriController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineCommercialSoft.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Models/Siniflar/Cariler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineCommercialSoft.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int Cariid { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(15)]
+        public string CariSehir { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+
+    }
+}
