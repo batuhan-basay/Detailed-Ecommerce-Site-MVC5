d7b7d7538eab16d1f3efbae188d0e443d09a1239
BatuhanBaşay <batuhan.basay@hotmail.com>
2022-04-07 03:37:37 +0300

    update

--- MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/KategoriController.cs
+++ MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/KategoriController.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineCommercialSoft.Models.Siniflar;
 
 namespace MvcOnlineCommercialSoft.Controllers
 {
     public class KategoriController : Controller
     {
+        Context c = new Context();
         // GET: Kategori
         public ActionResult Index()
         {
-
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+
+            return View("KategoriGetir", kategori);
+        }
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
