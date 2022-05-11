2ed54211501c23a4e567ab9e4c43b438ee19a925
BatuhanBaşay <batuhan.basay@hotmail.com>
2022-05-12 00:13:00 +0300

    Dinamik Faturalar ve Yetkilendirme

--- MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/DepartmanController.cs
+++ MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/DepartmanController.cs
@@ -17,7 +17,7 @@ namespace MvcOnlineCommercialSoft.Controllers
             var degerler = c.Departmans.Where(x => x.Durum == true).ToList();
             return View(degerler);
         }
-
+        [Authorize(Roles ="A")]
         [HttpGet]
         public ActionResult DepartmanEkle()
         {
--- MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/FaturaController.cs
+++ MvcOnlineCommercialSoft/MvcOnlineCommercialSoft/Controllers/FaturaController.cs
@@ -68,5 +68,41 @@ namespace MvcOnlineCommercialSoft.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Dinamik()
+        {
+            DinamikFatura cs = new DinamikFatura();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+
+            return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSıraNo, DateTime Tarih, string VergiDairesi, string Saat, string TeslimEden, string TeslimAlan, string Toplam,FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSıraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam =decimal.Parse(Toplam);
+
+            foreach (var x in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aciklama = x.Aciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.Faturaid = x.FaturaKalemid;
+                fk.Miktar = x.Miktar;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+
+            c.Faturalars.Add(f);
+            c.SaveChanges();
+            return Json("İşlem Başarılı",JsonRequestBehavior.AllowGet);
+        }
     }
 }
